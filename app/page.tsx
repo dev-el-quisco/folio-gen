e856a5522fb547dd2a132a4c2a8d37a2f4d12133
Sebastián Ignacio Castaneda Brito <scastaneda@elquisco.cl>
2025-05-19 12:25:04 -0400

    interfaz generador folios

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        q
+    <div className="flex min-h-screen grid-rows-[20px_1fr_20px] items-center justify-center p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col gap-5">
+        <span>
+          <h1 className="text-5xl leading-[1] font-bold">
+            Generador de Folios
+          </h1>
+          {/* <p className="text-lg leading-[1.5]">
+            Generador de folios para actas de entrega de ayudas asistenciales
+          </p> */}
+        </span>
+        <span className="grid grow grid-cols-2 gap-5">
+          <button className="h-11 grow rounded-lg bg-slate-800 px-8">
+            Generar
+          </button>
+          <p className="flex items-center rounded-lg border border-slate-700 px-3.5">
+            4CB2C53D-25-DO
+          </p>
+        </span>
       </main>
     </div>
   );
